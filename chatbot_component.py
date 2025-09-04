f7fa944b5ac3ae6c42f5bd17c1a1251b4251f1d0
Juan Pablo Pedraza Contreras <juanpedrazac@usantotomas.edu.co>
2025-09-04 11:23:41 -0500

    Update chatbot_component.py

--- chatbot_component.py
+++ chatbot_component.py
@@ -43,7 +43,7 @@ def show_chatbot():
             return f"Error: {e}"
 
     # === Entrada del usuario (Enter para enviar) ===
-    user_input = st.chat_input("Escribe tu pregunta de Ingeniería Electrónica...")
+    user_input = st.chat_input("Que dudas tienes sobre la expo.")
 
     if user_input:
         # Obtener respuesta
