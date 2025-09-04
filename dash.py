fce90b0333a78f99ec9462035d4cf0936ae2b6f3
Juan Pablo Pedraza Contreras <juanpedrazac@usantotomas.edu.co>
2025-09-04 11:06:34 -0500

    Update dash.py

--- dash.py
+++ dash.py
@@ -22,7 +22,7 @@ st.markdown(
         box-shadow: 0px 4px 10px rgba(0,0,0,0.15);
     }
     .card {
-        background-color: green;
+        background-color: green ligth;
         padding: 20px;
         border-radius: 15px;
         text-align: center;
